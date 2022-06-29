09da45c91cc1d021f70472c986e88b6a6beeb077
AlbertoMrMekko <albertomartinsanz1@gmail.com>
2022-06-29 20:54:33 +0200

    Commit variables

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=)");
         System.out.println("No + =(");
+        int x;
+        int y;
+        x = 2;
+        y = 7;
     }
 }
